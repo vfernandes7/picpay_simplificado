5507b15b0423666e0f970a55b4856b739b7019a7
Vinicius Fernandes <viniciusmfernandes@hotmail.com>
2024-10-04 07:03:15 -0300

    feat: create app config

--- src/main/java/com/pipaysimplificado/PipaysimplificadoApplication.java
+++ src/main/java/com/pipaysimplificado/PipaysimplificadoApplication.java
@@ -1,7 +1,10 @@
 package com.pipaysimplificado;
 
+import com.pipaysimplificado.services.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class PipaysimplificadoApplication {
