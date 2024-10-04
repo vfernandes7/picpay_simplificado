720329d90e21099e93b7440caf13489f98a83b94
Vinicius Fernandes <viniciusmfernandes@hotmail.com>
2024-10-04 07:00:46 -0300

    feat: update UserService and create transaction service

--- src/main/java/com/pipaysimplificado/services/UserService.java
+++ src/main/java/com/pipaysimplificado/services/UserService.java
@@ -2,11 +2,13 @@ package com.pipaysimplificado.services;
 
 import com.pipaysimplificado.domain.user.User;
 import com.pipaysimplificado.domain.user.UserType;
+import com.pipaysimplificado.dtos.UserDto;
 import com.pipaysimplificado.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 public class UserService {
@@ -27,6 +29,16 @@ public class UserService {
 
     public User findUserById(Long id) throws Exception {
         return this.repository.findUserById(id).orElseThrow(() -> new Exception("Usuário não encontrado"));
+    }
+
+    public User createUser(UserDto data){
+        User newUser = new User(data);
+        this.saveUser(newUser);
+        return newUser;
+    }
+
+    public List<User> getAllUsers(){
+        return this.repository.findAll();
     }
 
     public void saveUser(User user) {
